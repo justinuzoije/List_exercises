6bbcc5758c580725d2fc9eb1aad8223f04dcb735
Justin Uzoije <justinuzoije@gmail.com>
2017-02-09 15:33:06 -0500

    Enhanced matrix_addition functionality

--- matrix_addition.py
+++ matrix_addition.py
@@ -1,9 +1,3 @@
-#The number of rows in the matrix is 2
-matrixRowSize = 2
-
-#The number of columns in the matrix is 2
-matrixColumnSize = 2
-
 #[1,3]
 #[2,4]
 firstMatrix = [[1, 3],[2, 4]]
@@ -22,14 +16,14 @@ newMatrix = [[0, 0],[0, 0]]
 for i in range(len(firstMatrix)):
 #The second for loop is for accessing the data in the matrices themselves
 #The range includes 0 so we need one less than the matrix column size
-   for j in range(len(matrixColumnSize[0])):
+   for j in range(len(secondMatrix[0])):
 #The new matrix is made of adding each individual element of the original
 #matrices. Because it goes from i = 0, 1 and j = 0,1 it is accessing each
 #matrix as it goes through the loop
 
        newMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j]
 
-#[[First,0],[0,0]]  0,0
+#[[First,0],[0,0]]      0,0
 #[[0,Second],[0,0]]      0,1
 #[[0,0],[Third,0]]      1,0
 #[[0,0],[0,Fourth]]      1,1
@@ -39,29 +33,3 @@ for i in range(len(firstMatrix)):
 #   print(r)
 
 print newMatrix
-
-
-
-
-# # Program to add two matrices using nested loop
-#
-# X = [[12,7,3],
-#     [4 ,5,6],
-#     [7 ,8,9]]
-#
-# Y = [[5,8,1],
-#     [6,7,3],
-#     [4,5,9]]
-#
-# result = [[0,0,0],
-#          [0,0,0],
-#          [0,0,0]]
-#
-# # iterate through rows
-# for i in range(len(X)):
-#    # iterate through columns
-#    for j in range(len(X[0])):
-#        result[i][j] = X[i][j] + Y[i][j]
-#
-# for r in result:
-#    print(r)
